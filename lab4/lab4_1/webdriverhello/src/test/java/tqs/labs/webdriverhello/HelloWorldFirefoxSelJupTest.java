4d65c9e10f7818ceeecff916a562c6cc6d0d1edb
DiogoMMarto <diogo.marto@ua.pt>
2024-03-08 12:09:35 +0000

    lab 4_1

---
+++ lab4/lab4_1/webdriverhello/src/test/java/tqs/labs/webdriverhello/HelloWorldFirefoxSelJupTest.java
@@ -0,0 +1,26 @@
+package tqs.labs.webdriverhello;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ExtendWith(SeleniumJupiter.class)
+class HelloWorldFirefoxSelJupTest {
+
+    @Test
+    void test(ChromeDriver driver) {
+         // Exercise
+         String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
+         driver.get(sutUrl); 
+         String title = driver.getTitle(); 
+ 
+         // Verify
+         assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java"); 
+    }
+
+}
