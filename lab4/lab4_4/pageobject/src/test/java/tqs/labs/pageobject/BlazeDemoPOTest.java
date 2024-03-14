d0589e597fc8b69758f857d96ddfb79165b9c902
DiogoMMarto <diogo.marto@ua.pt>
2024-03-14 19:41:54 +0000

    lab 4_3 and 4_2

--- lab4/lab4_4/pageobject/src/test/java/tqs/labs/pageobject/BlazeDemoPOTest.java
+++ lab4/lab4_4/pageobject/src/test/java/tqs/labs/pageobject/BlazeDemoPOTest.java
@@ -3,20 +3,24 @@ package tqs.labs.pageobject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.hamcrest.MatcherAssert.assertThat;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.WebDriver;
 import static org.hamcrest.CoreMatchers.is;
 
+import io.github.bonigarcia.seljup.DockerBrowser;
 import io.github.bonigarcia.seljup.SeleniumJupiter;
 import tqs.labs.pageobject.PageObjects.ConfirmationPage;
 import tqs.labs.pageobject.PageObjects.HomePage;
 import tqs.labs.pageobject.PageObjects.PurchasePage;
 import tqs.labs.pageobject.PageObjects.ReservePage;
 
+import static io.github.bonigarcia.seljup.BrowserType.CHROME;
+
 @ExtendWith(SeleniumJupiter.class)
 class BlazeDemoPOTest{
 
     @Test
-    void test(ChromeDriver driver) {
+    void test(@DockerBrowser(type = CHROME) WebDriver driver) {
         
     HomePage home_page = new HomePage(driver);
     assertThat(home_page.title(), is("BlazeDemo"));
