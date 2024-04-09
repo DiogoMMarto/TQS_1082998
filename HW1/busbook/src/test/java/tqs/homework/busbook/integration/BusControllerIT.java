d995ecb129a1c15d08897d54ec5350fe4940ef36
DiogoMMarto <diogo.marto@ua.pt>
2024-04-10 00:28:28 +0100

    Report

--- HW1/busbook/src/test/java/tqs/homework/busbook/integration/BusControllerIT.java
+++ HW1/busbook/src/test/java/tqs/homework/busbook/integration/BusControllerIT.java
@@ -5,16 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-
-import tqs.homework.busbook.service.ConnectionService;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
